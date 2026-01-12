d6d683fcd84e0da2d22678ca62b562cd7adc0593
Mari Værnesbranden <maridubedare@gmail.com>
2026-01-12 12:55:17 +0100

    Flappy bird start

---
+++ module-5/Task_5-1_FlappyBird/hero.js
@@ -0,0 +1,31 @@
+"use strict";
+import { TSprite } from "libSprite";
+
+export class THero extends TSprite{
+    #gravity;
+    #speed;
+    constructor(aSpcvs, aSPI) {
+        const heroX = aSPI.background.width/2;
+        const heroY = (aSPI.background.height-aSPI.ground.height)/2;
+        super(aSpcvs, aSPI.hero1, heroX, heroY)
+        this.animationSpeed = 10;
+        this.#gravity = 9.81 / 100
+        this.#speed = 0;
+
+    }
+
+    animate(){
+        if(this.y < 400 - this.height){
+            this.#speed += this.#gravity;
+            this.y += this.#speed;
+            if(this.rotation < 90) {
+                this.rotation = this.#speed*20;
+            }
+        }
+    }
+
+    flap () {
+        this.#speed = -3;
+        this.rotation = 0;
+    }
+}
