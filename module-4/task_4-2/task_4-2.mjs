4f12918cbea3384682b6f74ab5d5887034188240
Mari Værnesbranden <maridubedare@gmail.com>
2026-03-20 15:44:38 +0100

    Task 4.2 start!

--- module-4/task_4-2/task_4-2.mjs
+++ module-4/task_4-2/task_4-2.mjs
@@ -2,52 +2,154 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create an array where you hard-code all the numbers from 1 to 20. Use a for loop to "run through" the
+array and print all the elements in the array.
+○ Hint: Look at the learning outcomes to find the solutions to the task.
+
+Put your code below here!*/
+const hardCodedArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+for(let i= 0; i < hardCodedArray.length; i++) {
+    printOut(i);
+}
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Take the array from task 1 and use a built-in method found in the array object to print all the elements with
+a custom defined character separating all the elements.
+○ Hint: You should be able to do it with just one line of code 😃
+
+Put your code below here!*/
+printOut(hardCodedArray.join("😃"));
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a constant that contains the text "Hei på deg, hvordan har du det?" (Hello there, how are you?)
+Take this text and convert it into an array that contains all the words in the text, i.e., each element should
+contain only one word from the text. Use a loop to traverse (run through) this array so that you can print
+which word number, which index the word is at, and the word itself.
+
+Put your code below here!*/
+
+let textPart3 = "Hei på deg, hvordan har du det?";
+
+const textConverted = textPart3.split(" ");
+
+for(let i=0; i < textConverted.length; i++) {
+    printOut("length: " + textConverted[i].length + ", index: " + i +  ", word: " + textConverted[i].toString());
+}
+//jeg skjønte ikke dette helt! skulle jeg ha index+1 for å finne word number? Jeg fikk det ikke til, får bare length
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create an array with these names: "Anne", "Inger", "Kari", "Marit", "Ingrid", "Liv", "Eva", "Berit", "Astrid",
+"Bjørg", "Hilde", "Anna", "Solveig", "Marianne", "Randi", "Ida", "Nina", "Maria", "Elisabeth" and "Kristin".
+Then create a function that will remove an element from an array. Let the function have two parameters.
+Parameter number one is the array from which you will remove the element, parameter two is the text that
+should be removed from the array. Check if the element exists in the array so that you can inform whether
+the element exists or not in the array.
+
+Put your code below here!*/
+
+let girlsArray = ["Anne", "Inger", "Kari", "Marit", "Ingrid", "Liv", "Eva", "Berit", "Astrid",
+"Bjørg", "Hilde", "Anna", "Solveig", "Marianne", "Randi", "Ida", "Nina", "Maria", "Elisabeth", "Kristin"]
+
+function removeName(aArray, aName) {
+    const index = aArray.indexOf(aName);
+    printOut("Name chosen: " + aName + newLine + "...");
+    if(aArray.includes(aName)) {
+        printOut(aName + " found! Removing name from array.")
+        aArray.splice(index, 1)
+    } else {
+        printOut(aName + " not found!")
+        return;
+    }
+}
+printOut(girlsArray.toString() + newLine);
+removeName(girlsArray, "Solveig")
+printOut(newLine + girlsArray.toString());
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a new array with these names: "Jakob", "Lucas", "Emil", "Oskar", "Oliver", "William", "Filip", "Noah",
+"Elias", "Isak", "Henrik", "Aksel", "Kasper", "Mathias", "Jonas", "Tobias", "Liam", "Håkon", "Theodor"and
+"Magnus" Merge the arrays with girl names and boy names into a new array with all the names.
+○ Hint: You can solve this with two lines of code. Remember that an empty array also has
+properties and methods 😃
+
+Put your code below here!*/
+
+let boysArray = ["Jakob", "Lucas", "Emil", "Oskar", "Oliver", "William", "Filip", "Noah",
+"Elias", "Isak", "Henrik", "Aksel", "Kasper", "Mathias", "Jonas", "Tobias", "Liam", "Håkon", "Theodor",
+"Magnus"]
+
+const girlsAndBoys = girlsArray.concat(boysArray);
+
+printOut(girlsAndBoys);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a class named TBook.
+Let the constructor fill in the three attributes (title, author, and ISBN number). Create a public function
+"toString" in the class, it should return a text string that contains the three attributes of the class.
+Create an array that contains three instances of the TBook class. Use a loop to print out the books that are
+in the list.
+
+Put your code below here!*/
+
+class TBook {
+
+    constructor(aTitle, aAuthor, aISBN) {
+
+    }
+
+    toString() {
+        
+    }
+}
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a static object!
+You can replace the language in the "name" attributes with whatever you want.
+Use this function: Object.keys(EWeekDays) to create an array with the "keys" that exist in the
+EWeekDays object.
+Create a loop that traverses this "key" array and prints all the elements that exist in the EWeekDays object
+○ Hint: Use W3Schools as I have shown you, here you see good examples of exactly this!
+
+Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create an array that contains 35 random numbers from 1 to 20 (inclusive). Sort these arrays in ascending
+and descending order. To get full credit for this task, it must be solved with "callback" functions that you
+use in the .sort(...) method of this array.
+
+Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Based on part 8, print out how many times the different numbers occur in the array. First, print the
+numbers and their frequency, then print the frequencies and which numbers they correspond to. You must
+print the most frequent ones first, and if there are multiple numbers where the frequency is the same, then
+it should again be sorted from the smallest to the largest number.
+
+Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create an array that contains rows and columns (2 dimensions, 5x9). Start with an empty array. Use "for"
+loops to create rows and columns, respectively. In each "cell," create a text that shows which row and
+column the "cell" is in. Then create two new sets of "for" loops to print the array itself.
+○ Hint: For each round in the loop for the rows, you create a column. And for each round in the
+columns, you write the "cell" value.
+
+Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
